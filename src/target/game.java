cd328b34211fc10993ec573e6ebdbaecd1682e9a
Jude95 <973829691@qq.com>
2015-05-13 10:54:37 +0800

    first connect

--- src/target/game.java
+++ src/target/game.java
@@ -1,10 +1,35 @@
 package target;
 
+import java.awt.Frame;
+import java.awt.Window;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
 public class game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("fuck!!!!!!!!!!!!!!!!!!!");
+		
+		try{
+			Socket socket=new Socket(args[0],Integer.parseInt(args[1]));
+			BufferedReader sin=new BufferedReader(new InputStreamReader(System.in));
+			PrintWriter os=new PrintWriter(socket.getOutputStream());
+			BufferedReader is=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String readline;
+			while(!readline.equals("bye")){
+				os.println(readline);
+				os.flush();
+				System.out.println("Client:"+readline);
+				System.out.println("Server:"+is.readLine());
+		}catch(Exception e) {
+		}
 	}
 
 }
