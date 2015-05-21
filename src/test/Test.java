af5f4dc7e572f7991e3aedc0c7941c8220c2231b
TooYouth <trieyouth@gmail.com>
2015-05-21 21:50:31 +0800

    i

---
+++ src/test/Test.java
@@ -0,0 +1,32 @@
+package test;
+
+import algorithms.statistics.Statistics;
+
+public class Test {
+
+	public static void main(String[] args) {
+		StateProducer s = new StateProducer(new Statistics());
+		StateJudger sj = new StateJudger();
+		for (int k = StateProducer.BEGIN; k <= StateProducer.END; k++) {
+			for (int t = StateProducer.BEGIN; t <= StateProducer.END; t++) {
+				for (int h = 0; h < StateProducer.KIND; h++) {
+					if (t == k) {
+						if (h == 1) {
+							continue;
+						}
+					}
+					s.initHold(k, t, h);
+					for (int i = 0; i < StateProducer.TIMES; i++) {
+						s.initCards();
+						int win = sj.getResult(s.getPokers());
+						s.setResult(win+1);
+						s.reset();
+					}
+					s.gameover();
+				}
+			}
+		}
+
+	}
+
+}
